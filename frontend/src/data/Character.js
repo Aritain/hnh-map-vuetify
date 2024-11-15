ca168722077f24c31ea41e7df837d56eaa558791
aritain <ganhart2@gmail.com>
2024-11-15 17:12:25 +0000

    revert change

--- frontend/src/data/Character.js
+++ frontend/src/data/Character.js
@@ -30,14 +30,7 @@ export class Character {
             let position = mapview.map.unproject([this.position.x, this.position.y], HnHMaxZoom);
 
             // Create a marker with a tooltip (player name)
-            this.marker = L.marker(position, {
-                icon: L.icon({
-                    iconUrl: this.iconUrl, // Use the current icon URL
-                    iconSize: [24, 24], // Replace with your icon size if different
-                    iconAnchor: [12, 12], // Center the icon
-                    tooltipAnchor: [0, -12] // Adjust to center tooltip above the icon
-                })
-            })
+            this.marker = L.marker(position)
                 .bindTooltip(this.name, { permanent: true, direction: 'top', opacity: 1 })
                 .on("click", this.callCallback.bind(this))
                 .addTo(mapview.map);
